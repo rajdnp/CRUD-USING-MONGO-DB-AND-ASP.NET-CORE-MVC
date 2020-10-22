c640ea70d8bd6b25c756b7e1467e9b3d25bbd22c
rajesh.ccin001 <rajesh.ccin001@gmail.com>
2020-10-22 12:32:30 +0530

    Add project files.

---
+++ CRUDUsingMongoDB/Controllers/StudentController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDUsingMongoDB.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace CRUDUsingMongoDB.Controllers
+{
+    public class StudentController : Controller
+    {
+        IMongoClient mongoClient;
+        IMongoDatabase database;
+        public StudentController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            var result = CRUDHelper.InsertRecord("College", "Students", new List<Student> {
+
+                new Student
+                {
+                    Name = "Jithendra"
+                },
+                new Student
+                {
+                    Name = "Rajesh"
+                },
+                new Student
+                {
+                    Name = "MALtech"
+                },new Student
+                {
+                    Name = "Jithendra"
+                },
+                new Student
+                {
+                    Name = "Rajesh"
+                },
+                new Student
+                {
+                    Name = "MALtech"
+                }
+
+            });
+
+            if (result.Equals(true))
+            {
+                return View();
+            }
+
+            return View("Error");
+
+        }
+
+        public IActionResult FindStudent()
+        {
+            return View();
+        }
+
+        [Route("emplist")]
+        public IActionResult ListStudents()
+        {
+            var result = CRUDHelper.ListRecords<Student>("College","Students");
+            return View(result);
+
+        }
+
+        public IActionResult EditStudent(Guid guid)
+        {
+            var result = CRUDHelper.LoadRecordById<Student>("College", "Students", guid);
+            return View(result);
+        }
+
+        public IActionResult DetailsStudent(Guid guid)
+        {
+            var result = CRUDHelper.LoadRecordById<Student>("College", "Students", guid);
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteStudent(Guid guid)
+        {
+            var result = CRUDHelper.LoadRecordById<Student>("College", "Students", guid);
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteStudent(Student student)
+        {
+            var result = CRUDHelper.DeleteRecordById<Student>("College", "Students", student.Id);
+            return RedirectToAction("ListStudents");
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent(Student student)
+        {
+            var result = CRUDHelper.UpdateRecordById<Student>("College", "Students", student.Id, student);
+            return RedirectToAction("ListStudents");
+        }
+
+    }
+
+    public class Student
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ CRUDUsingMongoDB/Helpers/CRUDHelper.cs
@@ -0,0 +1,89 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDUsingMongoDB.Helpers
+{
+    public static class CRUDHelper
+    {
+        private static IMongoClient client;
+        private static IMongoDatabase database;
+        public static bool InsertRecord<T>(string databasename, string table, List<T> entity)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(databasename);
+            var collection = database.GetCollection<T>(table);
+            var result = collection.InsertManyAsync(entity);
+            if (result.IsCompletedSuccessfully)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static List<T> ListRecords<T>(string databasename, string table)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(databasename);
+            var collection = database.GetCollection<T>(table);
+            var result = collection.Find(new BsonDocument()).ToList();
+            return result;
+        }
+
+        public static T LoadRecordById<T>(string databasename, string table, Guid guid)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(databasename);
+            var collection = database.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", guid);
+            return collection.Find(filter).FirstOrDefault();
+        }
+
+        public static async Task<bool> DeleteRecordById<T>(string databasename, string table, Guid guid)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(databasename);
+            var collection = database.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", guid);
+            var result = await collection.DeleteOneAsync(filter);
+
+            if (result.IsAcknowledged)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static async Task<bool> UpdateRecordById<T>(string databasename, string table, Guid guid, T entity)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(databasename);
+            var collection = database.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", guid);
+            try
+            {
+                var result = await collection.ReplaceOneAsync(filter, entity, new ReplaceOptions { IsUpsert = false });
+                if (result.IsAcknowledged)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+                throw;
+            }
+            return false;
+
+        }
+
+    }
+}
